c6e1a35257a1fc473555633ae22eee099244bad8
Shuma <shuma.h08@gmail.com>
2026-05-08 14:19:04 +0900

    use hash router

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,21 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import {
+  createRouter,
+  createHashHistory,
+} from "@tanstack/react-router";
+
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
-  routeTree,
-  context: { queryClient },
-  scrollRestoration: true,
-  defaultPreloadStaleTime: 0,
-  basepath: "/alche23",
-});
+    routeTree,
+    history: createHashHistory(),
+    context: { queryClient },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
 
   return router;
 };
